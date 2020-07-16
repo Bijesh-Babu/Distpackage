ab3ef3082e5c99eb85b5daa18ed48623e682ca54
Bijesh-Babu <61914762+Bijesh-Babu@users.noreply.github.com>
2020-07-16 23:02:18 +0100

    Add files via upload

---
+++ Q3 Alternative Solution.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Distribution:
+    
+    def Normal(mean, standard_deviation, size):
+        return np.random.normal(mean, standard_deviation, size)
+        
+    def Poisson(lamb, size): #with lamb the average rate of success
+        return np.random.poisson(lamb, size)
+    
+    def Binomial(n, p, size): #with n the number of trials and p the probability of success (with p between 0 and 1)
+        if p<0 or p>1:
+            raise Bound_exception("p needs to be between 0 and 1.") #raise an error if p is out of bound
+        else:
+            return np.random.binomial(n, p, size)
+
+    def Summarize(self):
+        print("Minimum:", np.min(self))
+        print("Maximum:", np.max(self))
+        print("Mean:", np.mean(self))
+        print("Standard deviation:", np.std(self))
+        
+    def Plot(self):
+        plt.hist(self)
+        
+class Bound_exception(Exception):
+    pass
+
---
+++ Setup- Package.py
@@ -0,0 +1,57 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from setuptools import setup\n",
+    "setup(name='Mangotestpackage',\n",
+    "version='0.1',\n",
+    "description='Testing installation of Package',\n",
+    "url='#',\n",
+    "author='Bijesh',\n",
+    "author_email='babubijesh@gmail.com',\n",
+    "license='MIT',\n",
+    "packages=['Mangotestpackage'],\n",
+    "zip_safe=False)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
