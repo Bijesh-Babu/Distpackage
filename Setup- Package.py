0931c06c1ed102bdd93d0fe7369b0ab1f6693279
Bijesh-Babu <61914762+Bijesh-Babu@users.noreply.github.com>
2020-07-16 23:08:25 +0100

    Update Setup- Package.py

--- Setup- Package.py
+++ Setup- Package.py
@@ -1,57 +1,24 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from setuptools import setup\n",
-    "setup(name='Mangotestpackage',\n",
-    "version='0.1',\n",
-    "description='Testing installation of Package',\n",
-    "url='#',\n",
-    "author='Bijesh',\n",
-    "author_email='babubijesh@gmail.com',\n",
-    "license='MIT',\n",
-    "packages=['Mangotestpackage'],\n",
-    "zip_safe=False)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+
+import setuptools
+
+setuptools.setup(
+    name='toms-dist-sampler',
+    version='0.0.1',
+    description=(
+        'A distribution sampler for Normal, Poisson and Binomial '
+        'distributions.'
+    ),
+    url='https://github.com/Bijesh-Babu/Distpackage',
+    author='Bijesh Babu',
+    author_email='babubijesh@gmail.com',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.7',
+        "Operating System :: OS Independent",
+    ]
+    
+) 
+    
